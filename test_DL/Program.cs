6bfeac21b647f7b2bbf2d54f9154b6599236aa31
wonyoung10 <yesdino06@gmail.com>
2025-09-04 15:07:30 +0900

    Add files via upload

---
+++ test_DL/Program.cs
@@ -0,0 +1,14 @@
+﻿//Load sample data
+using Test_DL;
+
+var sampleData = new MLModel1.ModelInput()
+{
+    //Col0 = @"Crust is not good.",
+    Col0 = "This restaurant was wonderful."
+};
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
